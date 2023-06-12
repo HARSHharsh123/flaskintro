a58b168ec0387bf22024bb54cf0a8afa2df675cc
HARSHharsh123 <hs0814497@gmail.com>
2023-06-12 12:46:43 +0530

    FlaskIntroCode

---
+++ PUSHinGithubCOde.py
@@ -0,0 +1,37 @@
+#fist of all we have to write 
+# 1 -> git init -> this initializes our code into 
+#github repo
+
+# 2. -> git add . -> this simply add our code into local
+
+#3 . -> git commit -m "My First Code"
+
+# 4. -> now next step me voh humse username or 
+#password magenga
+
+#4.1 git config --global user.email "hs0814497@gmail.com"
+#4.2 git config --global user.name "HARSHharsh123"
+
+#5. git branch -M main
+
+#6. git remote add origin https:// -> ek url hogi usko 
+#paste kar dena hai 
+
+#but it will give fatal error 
+#kiyoki hum kisi dusre origin me add karne ja rahe
+#hai apne code vaise bhee apke ek git ka origin phele se hi hota hai toh pehele usko 
+#remove karna padega
+
+#to remove write
+#git reomte remove origin
+
+#next
+#vapas git remote add origin https:// -> ek url yhi chala do no error 
+
+#then last 
+#git push -u origin main
+
+#now ye process hone ke baad vs code apki user name 
+#or email ko verify karega using authentication 
+#voh kuch code provide karega usko copy karke apko 
+#paste karna hai authentication ke vakat
